eab185da1c6b7cc03a4ac9842922ed3194de7a1a
Bouwe K. Westerdijk <bouwe@bouwe.nl>
2019-08-30 13:05:55 +0200

    .

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
+import useInterval from "./useInterval";
 
 function App() {
-  return <>hoi :)</>;
-}
+  let [count, setCount] = useState(0);
 
+  useInterval(() => {
+    setCount(count + 1);
+  }, 1000);
+
+  return <h1>{count}</h1>;
+}
 const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
